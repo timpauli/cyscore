18fb1515bee3c4e04f26688996b84a132b7ae034
inkeye <tim.pauli@posteo.de>
2017-11-06 20:06:15 +0100

    fixed clipping for example

--- examples/example.py
+++ examples/example.py
@@ -1,33 +1,38 @@
-from cyscore.voice import Voice
-from cyscore.score import Score
-from cyscore.note import Note
-
 from random import sample
 
+from cyscore.note import Note
+from cyscore.score import Score
+from cyscore.voice import Voice
 
-def gen(max_amp):
+
+def gen(max_amp, prev_dur):
     dist_pool = {0, 1, 2}
     dur_pool = {0.5, 1, 2}
     amp_pool = {0.25, 0.5, 1}
     pitch_pool = {50, 100, 150, 200, 250, 300}
 
+    new_max = 1
     dist = sample(dist_pool, 1)[0]
-    if dist == 0:
+    cond = dist < prev_dur
+    if cond:
         amp_pool = set(map(lambda x: x * max_amp, amp_pool))
     dur = sample(dur_pool, 1)[0]
     amp = sample(amp_pool, 1)
     pitch = sample(pitch_pool, 1)
 
-    return Note(dist, dur, amp + pitch)
+    if cond:
+        new_max = max_amp - amp[0]
+    return Note(dist, dur, amp + pitch), new_max
 
 
 n = 10
 
 notes = []
 max_amp = 1
+prev_dur = 0
 for i in range(n):
-    note = gen(max_amp)
-    max_amp = 1 - note.pfields[0]
+    note, max_amp = gen(max_amp, prev_dur)
+    prev_dur = note.duration
     notes.append(note)
 
 voice = Voice('example', notes)
