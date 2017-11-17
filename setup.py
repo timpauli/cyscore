c1129ea95aeaf2f35569dc399548cb09e892b0af
inkeye <tim.pauli@posteo.de>
2017-11-17 20:43:32 +0100

    new version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cyscore',
-    version='0.1.0',
+    version='0.1.1',
     description='score abstraction for csound written in python',
     author='Tim Pauli',
     author_email='tim.pauli@folkwang-uni.de',
