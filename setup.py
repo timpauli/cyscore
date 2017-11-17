15da0c93fded80916b0751aac670696c5df4aca6
inkeye <tim.pauli@posteo.de>
2017-11-17 20:39:51 +0100

    setup pypi readymade

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
-from setuptools import setup, find_packages
-
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    lic = f.read()
+from setuptools import setup
 
 setup(
     name='cyscore',
     version='0.1.0',
     description='score abstraction for csound written in python',
-    long_description=readme,
     author='Tim Pauli',
     author_email='tim.pauli@folkwang-uni.de',
     url='https://github.com/inkeye/cyscore',
-    license=lic,
     packages=['cyscore']
 )
