66cefe252dfff6cfefe4249f58e487ef6dc2d0ed
inkeye <tim.pauli@posteo.de>
2017-12-24 14:27:19 +0100

    starttime and duration for voices. tests needed

--- cyscore/voice.py
+++ cyscore/voice.py
@@ -5,10 +5,12 @@ from .note import Note
 
 
 class Voice:
-    def __init__(self, instr: str, notes: List[Note]) -> None:
+    def __init__(self, instr: str, notes: List[Note], starttime: float=0) -> None:
         assert(len(notes) > 0)
+        assert(starttime >= 0)
         self.__instr = instr
         self.__notes = notes
+        self.__starttime = starttime
 
     @property
     def instr(self) -> str:
@@ -17,6 +19,10 @@ class Voice:
     @property
     def notes(self) -> List[Note]:
         return list(self.__notes)
+
+    @property
+    def starttime(self) -> float:
+        return self.__starttime
 
     def __iter__(self) -> Iterator[Note]:
         return self.notes.__iter__()
@@ -30,6 +36,9 @@ class Voice:
         else:
             False
 
+    def duration(self):
+        return sum([n.delay for n in self.notes])
+
     def __eq(self, other: 'Voice') -> bool:
         i = self.instr == other.instr
         n = self.notes == other.notes
@@ -41,6 +50,7 @@ class Voice:
 
     def __repr__(self) -> str:
         times = accumulate(map(lambda x: x.delay, self.notes))
+        times = [time + self.starttime for time in times]
         block = self.__line(0, self.notes[0])
         for n, time in zip(self.notes[1:], times):
             block += '\n'
